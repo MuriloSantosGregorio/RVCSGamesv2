409045e9157181d703c30d2bb48c953b11d95475
edwaldpalmeira <edwaldpalmeira@hotmail.com>
2022-05-29 19:03:50 -0300

    Alteração interna do APP

--- app/src/main/java/com/example/rvcsgamesv2/MainActivity.kt
+++ app/src/main/java/com/example/rvcsgamesv2/MainActivity.kt
@@ -5,38 +5,39 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.rvcsgamesv2.databinding.ActivityMainBinding
+import com.example.rvcsgamesv2.inicio.InicioFragment
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
     lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         toggle = ActionBarDrawerToggle(this, binding.drawerLayout , R.string.abrir_menu, R.string.fechar_menu)
-
         binding.drawerLayout.addDrawerListener(toggle)
 
         toggle.syncState()
 
-        binding.nv.setNavigationItemSelectedListener {
+       binding.nv.setNavigationItemSelectedListener {
             when(it.itemId) {
-                R.id.ps4 -> {
-                    val frag = Ps4CFragment()
+                R.id.nav_inicio -> {
+                    val frag = InicioFragment()
                     supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
                 }
             }
             true
         }
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         toggle.onOptionsItemSelected(item)
         return super.onOptionsItemSelected(item)
     }
-
 }
--- app/src/main/java/com/example/rvcsgamesv2/jogos/Ps4CFragment.kt
+++ app/src/main/java/com/example/rvcsgamesv2/jogos/Ps4CFragment.kt
@@ -1,10 +1,11 @@
-package com.example.rvcsgamesv2
+package com.example.rvcsgamesv2.jogos
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.rvcsgamesv2.R
 
 class Ps4CFragment : Fragment() {
 
