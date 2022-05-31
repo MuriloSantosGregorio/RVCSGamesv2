2f1e4fbcdde8800a8b2c05f78e057601aa1f210c
murilo.sgregorio <murilosenacpi@gmail.com>
2022-05-31 20:36:11 -0300

    Adicionando Autenticacao Firebase

--- app/src/main/java/com/example/rvcsgamesv2/MainActivity.kt
+++ app/src/main/java/com/example/rvcsgamesv2/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.rvcsgamesv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import com.example.rvcsgamesv2.databinding.ActivityMainBinding
 import com.example.rvcsgamesv2.inicio.InicioFragment
 import com.example.rvcsgamesv2.jogos.Ps4CFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.AuthUI
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        tratarLogin()
 
         toggle = ActionBarDrawerToggle(this, binding.drawerLayout , R.string.abrir_menu, R.string.fechar_menu)
         binding.drawerLayout.addDrawerListener(toggle)
@@ -48,5 +54,27 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         toggle.onOptionsItemSelected(item)
         return super.onOptionsItemSelected(item)
+    }
+
+    fun tratarLogin() {
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            Toast.makeText(this, "Logado", Toast.LENGTH_SHORT).show()
+        } else {
+            val providers = arrayListOf(AuthUI.IdpConfig.EmailBuild().build())
+
+            val i = AuthUI.getInstance().createSignInItentBuilder().setAvaliableProviders(providers).build()
+
+            startActivityForResult(i,1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode ==1 && resultCode == RESULT_OK) {
+            Toast.makeText(this, "Autenticado", Toast.LENGTH_LONG).show()
+        } else {
+            finishAffinity()
+        }
     }
 }
