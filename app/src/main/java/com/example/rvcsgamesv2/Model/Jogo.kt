ebfd906573222f6b7751ec0249c4e27695a7e630
Murilo-Gregorio <murilosenacpi@gmail.com>
2022-06-01 23:55:19 -0300

    Adicionando o carrinho

---
+++ app/src/main/java/com/example/rvcsgamesv2/Model/Jogo.kt
@@ -0,0 +1,7 @@
+package com.example.rvcsgamesv2.Model
+
+data class Jogo(
+    var nome: String,
+    var preco: Float,
+    var descricao: String
+)
--- app/src/main/java/com/example/rvcsgamesv2/jogos/Ps4CFragment.kt
+++ app/src/main/java/com/example/rvcsgamesv2/jogos/Ps4CFragment.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.rvcsgamesv2.R
+import com.example.rvcsgamesv2.databinding.ActivityMainBinding
 
 class Ps4CFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+    lateinit var binding: ActivityMainBinding
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_ps4_c, container, false)
     }
 }
